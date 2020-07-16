fb64a07ac15c1d46359ba4123b657b769bf81e19
Waverley Leung <waverley.nyc@gmail.com>
2020-07-16 15:40:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,10 @@
 # Write your methods here
 def loop_message_five_times (string)
-  puts string
-  puts string
-  puts string
-  puts string
-  puts string
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
 end
 
 def loop_message_n_times (string, integer)
